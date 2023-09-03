92ab6a2bdb4c3f36e4fbd687d1b93c49809c5d8b
Joshua Van_Sant <joshua.vansant@outlook.com>
2023-09-02 22:33:03 -0600

    did some styling

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'package:geolocator/geolocator.dart' as geo;
 import 'package:google_directions_api/google_directions_api.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
-// void main() async => runApp(MyApp());
 void main() async {
   await dotenv.load(fileName: '.env');
   runApp(MyApp());
@@ -42,6 +40,92 @@ class _MapScreenState extends State<MapScreen> {
   Point? userCoords;
   String? eta;
 
+  @override
+  void initState() {
+    super.initState();
+    getUserLocation();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      fetchData();
+    });
+  }
+
+  @override
+  void dispose() {
+    _iotStream.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UCCS Shuttle Tracker'),
+        backgroundColor: Colors.black, 
+        centerTitle: true,
+        
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            
+            child: StreamBuilder<Point>(
+                stream: _iotStream.stream,
+                initialData: t1Coords,
+                builder: (BuildContext context, AsyncSnapshot<Point> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // log('waiting');
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    t1Coords = snapshot.data;
+                    return MapWidget(
+                      resourceOptions: ResourceOptions(
+                          accessToken: dotenv.env['MAPBOX_PUBLIC_ACCESS_TOKEN'] ??
+                              'Failed to load MapBox Access Token'),
+                      key: const ValueKey("mapWidget"),
+                      cameraOptions: CameraOptions(
+                          center: t1Coords!.toJson(), zoom: 14),
+                      onMapCreated: _onMapCreated,
+                    );
+                  } else {
+                    // log('else block - stream');
+                    return const Center(child: Text('No data available'));
+                  }
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RichText(
+                text: TextSpan(
+
+                    text: 'Shuttle 1\t|\t',
+                    style: const TextStyle(
+                        fontSize: 19.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        backgroundColor: Colors.black),
+                    children: <TextSpan>[
+                  const TextSpan(
+                      text: 'XshuttleStop\t|\t',
+                      style: TextStyle(
+                          fontSize: 19.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold)),
+                  TextSpan(
+                      text: eta,
+                      style: const TextStyle(
+                          fontSize: 19.0,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold))
+                ])),
+          ),
+        ],
+      ),
+    );
+  }
+
   Future<Position> getUserLocation() async {
     var permission = await geo.Geolocator.checkPermission();
     if (permission == geo.LocationPermission.denied) {
@@ -60,16 +144,17 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> fetchData() async {
-    final response = await http.get(
-        Uri.parse(
-            'https://api.init.st/data/v1/events/latest?accessKey=ist_rg6P7BFsuN8Ekew6hKsE5t9QoMEp2KZN&bucketKey=jmvs_pts_tracker'));
+    final response = await http.get(Uri.parse(
+        'https://api.init.st/data/v1/events/latest?accessKey=ist_rg6P7BFsuN8Ekew6hKsE5t9QoMEp2KZN&bucketKey=jmvs_pts_tracker'));
 
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
-      final lng = double.parse(
-          jsonResponse['tracker1']['value'].toString().split(',')[0]);
-      final lat = double.parse(
-          jsonResponse['tracker1']['value'].toString().split(',')[1]);
+      final lng = double.parse(jsonResponse['tracker1']['value']
+          .toString()
+          .split(',')[0]);
+      final lat = double.parse(jsonResponse['tracker1']['value']
+          .toString()
+          .split(',')[1]);
       final coords = Point(coordinates: Position(lat, lng));
       t1Coords = coords;
       _iotStream.add(coords);
@@ -78,18 +163,19 @@ class _MapScreenState extends State<MapScreen> {
       // Calculate ETA
       final origin = '${t1Coords!.coordinates.lat}, ${t1Coords!.coordinates.lng}';
       final destination = '38.89226825273266, -104.79764917960803';
-      DirectionsService.init(dotenv.env['DIRECTIONS_API_KEY'] ?? 'Failed to load Directions API Key');
+      DirectionsService.init(
+          dotenv.env['DIRECTIONS_API_KEY'] ?? 'Failed to load Directions API Key');
       final directionsService = DirectionsService();
-      
+
       final request = DirectionsRequest(
         origin: origin,
         destination: destination,
         travelMode: TravelMode.driving,
-        // waypoints: [
-        //   DirectionsWaypoint(location: '38.89122033268761, -104.79908324703885'),
-        //   DirectionsWaypoint(location: '38.89154559249909, -104.79803199145594'),
-        //   // DirectionsWaypoint(location: LatLng(38.892084300785534, -104.79797975515368)),
-        //   ],
+        waypoints: [
+          DirectionsWaypoint(location: '38.89122033268761, -104.79908324703885'),
+          DirectionsWaypoint(location: '38.89154559249909, -104.79803199145594'),
+          // DirectionsWaypoint(location: LatLng(38.892084300785534, -104.79797975515368)),
+        ],
       );
 
       directionsService.route(request,
@@ -97,13 +183,10 @@ class _MapScreenState extends State<MapScreen> {
         if (status == DirectionsStatus.ok) {
           final route = response.routes!.first;
           final duration = route.legs!.first.duration;
-          // print('ETA: ${duration!.value.toString()} seconds');
           setState(() {
-              eta = '${duration!.value.toString()} seconds';
-
+            eta = '${duration!.value.toString()} seconds';
           });
         } else {
-          // print('Error: $status');
           eta = 'Error: $status';
         }
       });
@@ -116,15 +199,16 @@ class _MapScreenState extends State<MapScreen> {
     final ByteData bytes = await rootBundle.load('assets/userLocation.png');
     final Uint8List list = bytes.buffer.asUint8List();
     if (tracker1 == null) {
-      pointAnnotationManager?.create(
-          PointAnnotationOptions(
+      pointAnnotationManager?.create(PointAnnotationOptions(
+              textField: 'Shuttle 1',
+              textOffset: [0, 1.25],
               geometry: t1Coords!.toJson(),
-              iconSize: .5,
+              iconSize: .3,
               symbolSortKey: 10,
               image: list))
           .then((value) => tracker1 = value);
     } else {
-      var point = Point.fromJson((tracker1!.geometry)!.cast());
+      Point.fromJson((tracker1!.geometry)!.cast());
       var newPoint = t1Coords!.toJson();
       tracker1?.geometry = newPoint;
       pointAnnotationManager?.update(tracker1!);
@@ -134,73 +218,13 @@ class _MapScreenState extends State<MapScreen> {
 
   _onMapCreated(MapboxMap mapboxMap) {
     this.mapboxMap = mapboxMap;
-    this.mapboxMap!.location
-        .updateSettings(LocationComponentSettings(enabled: true)); // show current position
+    this.mapboxMap!.gestures.updateSettings(GesturesSettings(rotateEnabled: false));
+    this.mapboxMap!.location.updateSettings(LocationComponentSettings(enabled: true)); // show current position
     mapboxMap.annotations.createPointAnnotationManager().then((value) async {
       pointAnnotationManager = value;
     });
     setState(() {
       _createT1Marker();
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getUserLocation();
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      fetchData();
-    });
-  }
-
-  @override
-  void dispose() {
-    _iotStream.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Map Screen'),
-        ),
-        body: Column(children: [Expanded(child: StreamBuilder<Point>(
-            stream: _iotStream.stream,
-            initialData: t1Coords,
-            builder: (BuildContext context, AsyncSnapshot<Point> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                log('waiting');
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                t1Coords = snapshot.data;
-                return MapWidget(
-                  resourceOptions: ResourceOptions(
-                    accessToken:
-                        // '',
-                        dotenv.env['MAPBOX_PUBLIC_ACCESS_TOKEN'] ?? 'Failed to load MapBox Access Token'
-                  ),
-                  key: ValueKey("mapWidget"),
-                  cameraOptions: CameraOptions(
-                      center: t1Coords!.toJson(), zoom: 11),
-                  onMapCreated: _onMapCreated,
-                );
-              } else {
-                log('else block - stream');
-                return Center(child: Text('No data available'));
-              }
-            })
-            ), Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'ETA: $eta',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-            ]
-            ,) 
-            );
   }
 }
